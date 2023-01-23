19f6f2709560253b78ea1a7407abaeaddb578abc
criedley <creedley@gmail.com>
2023-01-22 22:56:29 -0500

    Add game logic
    Add function game() that plays multiple rounds and reports winner best 3/5

--- rock-paper-scissors/scripts.js
+++ rock-paper-scissors/scripts.js
@@ -16,11 +16,61 @@ function getCompChoice() {
 }
 
 function getPlayerChoice() {
-    return prompt("Enter 'rock', 'paper', or 'scissors': ");
+    let playerChoice = prompt("Enter 'rock', 'paper', or 'scissors': ");
+    return playerChoice.toLowerCase();
 }
 
 function playRound() {
-    playerChoice = getPlayerChoice();
+    let playerChoice = getPlayerChoice();
+    let compChoice = getCompChoice();
+
+    if (playerChoice === "rock") {
+        if (compChoice === "rock") {
+            return "Tie!";
+        } else if (compChoice === "paper") {
+            return "Computer choses paper. You lose!";
+        } else if (compChoice === "scissors") {
+            return "Computer chooses scissors. You win!";
+        }
+    } else if (playerChoice === "paper") {
+        if (compChoice === "rock") {
+            return "Computer chooses rock. You win!";
+        } else if (compChoice === "paper") {
+            return "Computer choses paper. It's a tie!";
+        } else if (compChoice === "scissors") {
+            return "Computer chooses scissors. You loose!";
+        }
+    } else if (playerChoice === "scissors") {
+        if (compChoice === "rock") {
+            return "Computer chooses rock. You loose!";
+        } else if (compChoice === "paper") {
+            return "Computer choses paper. You win!";
+        } else if (compChoice === "scissors") {
+            return "Computer chooses scissors. It's a tie!";
+        }
+    }
 }
 
-playRound();
+function game() {
+    let compWins = 0;
+    let playerWins = 0;
+
+    for (let round = 0; round < 5; round++) {
+        let roundOutcome = playRound();
+
+        if (roundOutcome.search("win") > 0) {
+            playerWins++;
+        } else {
+            compWins++;
+        }
+        console.log(roundOutcome);
+    }
+    console.log(`You won ${playerWins} out of 5 rounds.`);
+    if (playerWins >= 3) {
+        console.log("You won the game!");
+    } else {
+        console.log("You lost the game!");
+    }
+}
+
+game();
